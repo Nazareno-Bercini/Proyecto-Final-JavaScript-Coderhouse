f6153d4b0ab7252fe8ebd7cef71a7d124ef55dcb
Nazareno Bercini <89424150+Nazareno-Bercini@users.noreply.github.com>
2022-06-02 19:55:56 -0300

    Primera entrega proyecto final

--- main.js
+++ main.js
@@ -1,113 +1,83 @@
 document.addEventListener('DOMContentLoaded', function() {
-    //Declaro variables
-    
-    let puntosAFavor  = 0;
-    let puntosEnContra = 0;
-    let rondas = 5
-    
-    function resultadoFinal(puntosAFavor, puntosEnContra){
-        return puntosAFavor - puntosEnContra;
-    }
-    
-    alert("Bienvenido! Tendras que sumar puntos en la proximas 5 rondas")
-    
-    //Indicar la cantidad de puntos a favor por cinco rondas
-    
-    for(let i=1; i<=rondas; i++){
-        alert("Ronda N° " + i)
-        
 
-        let puntosGanados = prompt('Cantidad de puntos ganados: ');
-
-      while (isNaN(puntosGanados) == true || parseInt(puntosGanados) < 0 || puntosGanados == null || puntosGanados.length == 0 || /^\s+$/.test(puntosGanados)) {
-       alert('No es válido. Sólo ingrese numéros mayores a 0.');
-       puntosGanados = prompt('Indique la cantidad de puntos ganados:');
+  class Partidos {
+      constructor(){
+          this.resultados = [ ];
       }
-        puntosAFavor += parseInt(puntosGanados);
-
-
-
-
-        // let puntosGanados = prompt("Cantidad de puntos ganados: ");
-        // puntosAFavor += parseInt(puntosGanados);
-        
-        // while (isNaN(puntosGanados) == true || parseInt(puntosGanados) < 0 || puntosGanados == null || puntosGanados.length == 0 || /^\s+$/.test(puntosGanados)){
-        //     alert("No es válido. Sólo ingrese numéros mayores a 0.");
-        //     puntosGanados = prompt("Indique la cantidad de puntos ganados: ");
-        //     if (isNaN(puntosGanados) == false && (parseInt(puntosGanados) > 0)){
-        //     }
-        //     puntosAFavor += parseInt(puntosGanados);
-        //     break;
-        // }
-    }
- 
-//Indicar la cantidad de puntos en contra por cinco rondas
- 
-for(let i=1; i<=rondas; i++){
-    alert("Ronda N° " + i)
-
-    let puntosPerdidos = prompt('Cantidad de puntos perdidos: ');
-
-      while (isNaN(puntosPerdidos) == true || parseInt(puntosPerdidos) < 0 || puntosPerdidos == null || puntosPerdidos.length == 0 || /^\s+$/.test(puntosPerdidos)) {
-       alert('No es válido. Sólo ingrese numéros mayores a 0.');
-      puntosPerdidos = prompt('Indique la cantidad de puntos perdidos:');
+      
+      agregarResultados(resultados){
+        this.resultados.push(resultado)
+  
       }
-    puntosEnContra += parseInt(puntosPerdidos);
-
-
-
-
-
-
- 
-    //     let puntosPerdidos = prompt("Cantidad de puntos perdidos: ");
-    //     puntosEnContra += + parseInt(puntosPerdidos);
- 
-    //     while (isNaN(puntosPerdidos) == true || parseInt(puntosPerdidos) < 0 || puntosPerdidos == null || puntosPerdidos.length == 0 || /^\s+$/.test(puntosPerdidos)){
-    //         alert("No es válido. Sólo ingrese numéros mayores a 0.");
-    //         puntosPerdidos = prompt("Indique la cantidad de puntos perdidos:");
-    //         if (isNaN(puntosPerdidos) == false && (parseInt(puntosPerdidos) > 0)){
-    //             puntosEnContra += parseInt(puntosPerdidos);
-    //             break;
-    //         }
-    //     }
-    }
+  
+  }
   
 
+// Le asigno a cada grupo 4 equipos 
 
-
-    function puntos(partidos){
+  const grupoA =["Qatar", "Paises Bajos", "Senegal", "Ecuador" ];
+  const grupoB =["Inglaterra", "EEUU", "Iran", "Gales" ];
+  const grupoC =["Argentina", "Mexico", "Polonia", "Arabia" ];
+  const grupoD =["Francia", "Dinamarca", "Tunez", "Peru" ];
+  const grupoE =["España", "Alemania", "Japon", "Nueva Zelanda" ];
+  const grupoF =["Belgica", "Croacia", "Marruecos", "Canada" ];
+  const grupoG =["Brasil", "Serbia", "Suiza", "Camerun" ];
+  const grupoH =["Portugal", "Uruguay", "Corea", "Ghana" ];
+  
+  
+  let encontrado = grupoC.filter((equipo) => equipo.includes("Ar"));
+  console.log(encontrado);
+  
+  
+  
+  let golesAFavor  = [];
+  let golesEnContra = [];
+  let partidos = 3
+  
+  
+  // Indico la cantidad de goles por partido
+  
+  for(let i=1; i<=partidos; i++){
+      alert("Partido N° " + i)
+     
+      let golesConvertidos = prompt('Cantidad de goles convertidos: ');
+      let golesRecibidos = prompt('Cantidad de goles recibidos: ');
+      
+      while (isNaN(golesConvertidos) == true || parseInt(golesConvertidos) < 0 || golesConvertidos == null || golesConvertidos.length == 0 || /^\s+$/.test(golesConvertidos)) {
+          alert('No es válido. Sólo ingrese numéros mayores a 0.');
+          golesConvertidos = prompt('Indique la cantidad de goles convertidos: ');
+            break;
+      
+      }
+      golesAFavor.push(golesConvertidos);
+     
+      while (isNaN(golesRecibidos) == true || parseInt(golesRecibidos) < 0 || golesRecibidos == null || golesRecibidos.length == 0 || /^\s+$/.test(golesRecibidos)) {
+          alert('No es válido. Sólo ingrese numéros mayores a 0.');
+          golesEnContra = prompt('Indique la cantidad de goles recibidos: ');
+            break;
+        }
+      golesEnContra.push(golesRecibidos);
+  
+  }
+  
+  function resultadoFinal(golesAFavor, golesEnContra){
+    let resultado = golesAFavor + ":" + golesEnContra
+      console.log(resultado);
+  }
+  //Saco la cantidad de puntos ganados por equipo
+  
+  function puntos(resultado){
       let cantPuntos = 0
-  
-      for (let i = 0; i < partidos.lenght; i++){
-          if(partidos[i][0] > partidos[i][2]) cantPuntos += 3
-          else if(partidos[i][0] < partidos[i][2]) cantPuntos += 0
-          else if(partidos[i][0] == partidos[i][2]) cantPuntos += 1
+      
+      for (let i = 0; i < resultado.lenght; i++){
+          if(resultadoFinal[i][0] > resultado[i][2]) cantPuntos += 3
+          else if(resultadoFinal[i][0] < resultado[i][2]) cantPuntos += 0
+          else if(resultadoFinal[i][0] == resultado[i][2]) cantPuntos += 1
       }
   
-      return cantPuntos;
-
-    }
- 
-//Dar el total de puntos
- 
-// alert("Cantidad de puntos totales obtenidos: " + puntosAFavor);
-// alert("Perdiste " + puntosEnContra + " puntos a lo largo de las rondas");
-// alert("Por lo tanto, tu resultado final es: " + resultadoFinal(puntosAFavor, puntosEnContra))
-    
-}, false);   
+      console.log(cantPuntos);
+  }
 
 
-
-
-
-// alert('Ronda N° ' + i);
-// let puntosPerdidos = prompt('Cantidad de puntos perdidos: ');
-
-//       while (isNaN(puntosPerdidos) == true || parseInt(puntosPerdidos) < 0 || puntosPerdidos == null || puntosPerdidos.length == 0 || /^\s+$/.test(puntosPerdidos)) {
-//        alert('No es válido. Sólo ingrese numéros mayores a 0.');
-//       puntosPerdidos = prompt('Indique la cantidad de puntos perdidos:');
-//       }
-// puntosEnContra += parseInt(puntosPerdidos);
-
-
+  
+  }, false);   
