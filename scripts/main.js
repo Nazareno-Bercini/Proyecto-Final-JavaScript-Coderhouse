a62aa820036d120ba9cfcc592df1733e5fd1f06d
Nazareno Bercini <89424150+Nazareno-Bercini@users.noreply.github.com>
2022-06-23 22:12:52 -0300

    Incorporando librerias

--- scripts/main.js
+++ scripts/main.js
@@ -5,7 +5,7 @@ document.addEventListener('DOMContentLoaded', function() {
     const nombre = document.getElementById('nombre')
     const email = document.getElementById('email')
     const contrasena = document.getElementById('contrasena')
-    const resgistro = document.getElementById('registro')
+    const form = document.getElementById('registro')
     const warnings = document.getElementById('warnings')
 
     form.addEventListener('submit', e =>{
@@ -13,7 +13,7 @@ document.addEventListener('DOMContentLoaded', function() {
         let warnings = ''
         let entrar = false
         let regexEmail = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/
-        parrafo.innerHTML = ''
+        warnings.innerHTML = ''
         if (nombre.value.length <= 7){
             warnings += 'El nombre debe tener mas de 6 caracteres <br>'
             entrar = true
@@ -27,40 +27,36 @@ document.addEventListener('DOMContentLoaded', function() {
             entrar = true
         }
         if(entrar) {
-            parrafo.innerHTML = warnings
+            warnings.innerHTML = warnings
         } else{
-            parrafo.innerHTML = "Registrado"
+            warnings.innerHTML = "Registrado"
         }
 
     })
+    let boton = document.getElementById('botonform')
+    boton.addEventListener('click',()=>{
+
+      
+      Swal.fire({
+        title: 'Confirmar registro',
+        icon: 'question',
+        showCancelButton: true,
+        confirmButtonColor: '#3085d6',
+        cancelButtonColor: '#d33',
+        confirmButtonText: 'Si, quiero registrarme'
+      }).then((result) => {
+        if (result.isConfirmed) {
+          Swal.fire(
+            'Registrado!',
+            'Bienvenido a nuestra pagina.',
+            'success'
+          )
+        }
+      })
     
-    // cuando se pulsa en "agregar a favoritos"
-document.getElementById("seleccion").addEventListener("click", function(e) {
+    })
+      
 
-    e.preventDefault();
-  
-    // leo el id del equipo y lo guardo en un objeto
-    let datos = {
-      id: document.getElementById('selec1', 'selec2', 'selec3', 'selec4').value,
-    };
-  
-    // leo los favoritos del localStorage
-    let favoritos = localStorage.getItem("favoritos") || "[]";
-    favoritos = JSON.parse(favoritos);
-  
-    // busco el equipo en la lista de favoritos
-    let posLista = favoritos.findIndex(function(e) { return e.id == datos.id; });
-    if (posLista > -1) {
-      // si está, lo quito
-      favoritos.splice(posLista, 1);
-    } else {
-      // si no está, lo añado
-      favoritos.push(datos);
-    }
-// guardo la lista de favoritos 
-localStorage.setItem("favoritos", JSON.stringify(favoritos));
-
-});  
 
     // declaro las constantes para el fixture
     
@@ -76,13 +72,31 @@ localStorage.setItem("favoritos", JSON.stringify(favoritos));
   
     btnUno.onclick = () =>{
       if(argentina.value > arabia.value){
-        alert('Gano Argentina')
+        Swal.fire({
+          position: 'top-end',
+          icon: 'success',
+          title: 'Argentina gano. Se lleva los 3 puntos',
+          showConfirmButton: false,
+          timer: 2000
+        })
       } else{
         if (argentina.value == arabia.value){
-          alert ('Empate')
+          Swal.fire({
+            position: 'top-end',
+            icon: 'warning',
+            title: 'Argentina empato. Se lleva 1 punto',
+            showConfirmButton: false,
+            timer: 2000
+          })
         } else{
           if (argentina.value < arabia.value){
-            alert ('Gano Arabia Saudita')
+            Swal.fire({
+              position: 'top-end',
+              icon: 'error',
+              title: 'Argentina perdio. No se lleva ningun punto',
+              showConfirmButton: false,
+              timer: 2000
+            })
           }
         }
       }
@@ -90,13 +104,31 @@ localStorage.setItem("favoritos", JSON.stringify(favoritos));
   
     btnDos.onclick = () =>{
       if(argentinaDos.value > mexico.value){
-        alert('Gano Argentina')
+        Swal.fire({
+          position: 'top-end',
+          icon: 'success',
+          title: 'Argentina gano. Se lleva los 3 puntos',
+          showConfirmButton: false,
+          timer: 2000
+        })
       } else{
         if (argentinaDos.value == mexico.value){
-          alert ('Empate')
+          Swal.fire({
+            position: 'top-end',
+            icon: 'warning',
+            title: 'Argentina empato. Se lleva 1 punto',
+            showConfirmButton: false,
+            timer: 2000
+          })
         } else{
           if (argentinaDos.value < mexico.value){
-            alert ('Gano Mexico')
+            Swal.fire({
+              position: 'top-end',
+              icon: 'error',
+              title: 'Argentina perdio. No se lleva ningun punto',
+              showConfirmButton: false,
+              timer: 2000
+            })
           }
         }
       }
@@ -104,18 +136,36 @@ localStorage.setItem("favoritos", JSON.stringify(favoritos));
   
     btnTres.onclick = () =>{
       if(argentinaTres.value > polonia.value){
-        alert('Gano Argentina')
+        Swal.fire({
+          position: 'top-end',
+          icon: 'success',
+          title: 'Argentina gano. Se lleva los 3 puntos',
+          showConfirmButton: false,
+          timer: 2000
+        })
       } else{
         if (argentinaTres.value == polonia.value){
-          alert ('Empate')
+          Swal.fire({
+            position: 'top-end',
+            icon: 'warning',
+            title: 'Argentina empato. Se lleva 1 punto',
+            showConfirmButton: false,
+            timer: 2000
+          })
         } else{
           if (argentinaTres.value < polonia.value){
-            alert ('Gano Polonia')
+            Swal.fire({
+              position: 'top-end',
+              icon: 'error',
+              title: 'Argentina perdio. No se lleva ningun punto',
+              showConfirmButton: false,
+              timer: 2000
+            })
           }
         }
       }
     }
-
+  
 
   
   }, false);   
